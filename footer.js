a6cf89d97b7b9b6c66e233ec82f216e39c18eb9c
trultinfo-creator <trultinfo@gmail.com>
2026-05-27 18:18:12 -0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footer = document.createElement("footer");
-    footer.style.cssText = "margin-top:auto; padding:40px 0; font-size:0.7rem; color:#555; text-transform:uppercase; letter-spacing:2px;";
-    footer.innerHTML = "&copy; 2026 SUBLIME MANAGEMENT GROUP";
+    footer.style.cssText = "margin-top:auto; padding:40px 0; font-size:0.7rem; color:#555; text-transform:uppercase; letter-spacing:2px; text-align:center;";
+    footer.innerHTML = "&copy; 2026 SUBLIME MANAGEMENT GROUP. ALL RIGHTS RESERVED.";
     document.body.appendChild(footer);
 });
+
